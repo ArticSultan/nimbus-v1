0e6a5f6aabe7b80ccef5c3c1d091022602c127ba
ArticSultan <141678081+ArticSultan@users.noreply.github.com>
2023-12-07 21:28:36 -0800

    Add files via upload

---
+++ socket_client.c
@@ -0,0 +1,236 @@
+#if defined(_WIN32)
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+#endif
+
+#if defined(_WIN32)
+#define ISVALIDSOCKET(s) ((s) != INVALID_SOCKET)
+#define CLOSESOCKET(s) closesocket(s)
+#define GETSOCKETERRNO() (WSAGetLastError())
+
+#else
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+#endif
+
+
+#include <openssl/crypto.h>
+#include <openssl/x509.h>
+#include <openssl/pem.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]) {
+
+#if defined(_WIN32)
+    WSADATA d;
+    if (WSAStartup(MAKEWORD(2, 2), &d)) {
+        fprintf(stderr, "Failed to initialize.\n");
+        return 1;
+    }
+#endif
+
+    if (argc < 3) {
+        fprintf(stderr, "usage: tcp_client hostname port\n");
+        return 1;
+    }
+
+    printf("Configuring remote address...\n");
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_socktype = SOCK_STREAM;
+    struct addrinfo *peer_address;
+    if (getaddrinfo(argv[1], argv[2], &hints, &peer_address)) {
+        fprintf(stderr, "getaddrinfo() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+
+    printf("Remote address is: ");
+    char address_buffer[100];
+    char service_buffer[100];
+    getnameinfo(peer_address->ai_addr, peer_address->ai_addrlen,
+            address_buffer, sizeof(address_buffer),
+            service_buffer, sizeof(service_buffer),
+            NI_NUMERICHOST);
+    printf("%s %s\n", address_buffer, service_buffer);
+
+
+    printf("Creating socket...\n");
+    SOCKET socket_peer;
+    socket_peer = socket(peer_address->ai_family,
+            peer_address->ai_socktype, peer_address->ai_protocol);
+    if (!ISVALIDSOCKET(socket_peer)) {
+        fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+
+    printf("Connecting...\n");
+    if (connect(socket_peer,
+                peer_address->ai_addr, peer_address->ai_addrlen)) {
+        fprintf(stderr, "connect() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+    freeaddrinfo(peer_address);
+
+    printf("Connected.\n");
+    printf("To send data, enter text followed by enter.\n");
+
+    while(1) {
+
+        fd_set reads;
+        FD_ZERO(&reads);
+        FD_SET(socket_peer, &reads);
+#if !defined(_WIN32)
+        FD_SET(0, &reads);
+#endif
+
+        struct timeval timeout;
+        timeout.tv_sec = 0;
+        timeout.tv_usec = 100000;
+
+        /*
+        SOCKET max_socket;
+        max_socket = socket_listen;
+        if (socket_a > max_socket) max_socket = socket_a;
+        if (socket_b > max_socket) max_socket = socket_b;
+
+        fd_set copy;
+        copy = our_sockets;
+
+        SOCKET i;
+        for(i = 1; i <= max_socket; ++i) {
+            if (FD_ISSET(i, &copy)) {
+                HANDLE socket
+                if (i == socket_listen)
+            }
+        }   
+
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
+
+    SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
+    if (!ctx) {
+        fprintf(stderr, "SSL_CTX_new() failed.\n");
+        return 1;
+    }
+ SSL *ssl = SSL_new(ctx);
+    if (!ssl) {
+        fprintf(stderr, "SSL_new() failed.\n");
+        return 1;
+    }
+
+    if (!SSL_set_tlsext_host_name(ssl, hostname)) {
+        fprintf(stderr, "SSL_set_tlsext_host_name() failed.\n");
+        ERR_print_errors_fp(stderr);
+        return 1;
+    }
+
+    SSL_set_fd(ssl, server);
+    if (SSL_connect(ssl) == -1) {
+        fprintf(stderr, "SSL_connect() failed.\n");
+        ERR_print_errors_fp(stderr);
+        return 1;
+    }
+
+     printf ("SSL/TLS using %s\n", SSL_get_cipher(ssl));
+
+ SSL_write(ssl, buffer, strlen(buffer));
+ while(1) {
+        int bytes_received = SSL_read(ssl, buffer, sizeof(buffer));
+            if (bytes_received < 1) {
+                printf("\nConnection closed by peer.\n");
+                break;
+            }
+
+            printf("Received (%d bytes): '%.*s'\n",
+                    bytes_received, bytes_received, buffer);
+
+    } //end while(1)
+
+    printf("\nClosing socket...\n");
+    SSL_shutdown(ssl);
+    CLOSESOCKET(server);
+    SSL_free(ssl);
+    SSL_CTX_free(ctx);
+
+
+X509 *cert = SSL_get_peer_certificate(ssl);
+    if (!cert) {
+        fprintf(stderr, "SSL_get_peer_certificate() failed.\n");
+        return 1;
+    }
+
+    char *tmp;
+    if ((tmp = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0))) {
+        printf("subject: %s\n", tmp);
+        OPENSSL_free(tmp);
+    }
+
+    if ((tmp = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0))) {
+        printf("issuer: %s\n", tmp);
+        OPENSSL_free(tmp);
+    }
+
+    X509_free(cert);
+ 
+        */
+
+        if (select(socket_peer+1, &reads, 0, 0, &timeout) < 0) {
+            fprintf(stderr, "select() failed. (%d)\n", GETSOCKETERRNO());
+            return 1;
+        }
+
+        if (FD_ISSET(socket_peer, &reads)) {
+            char read[4096];
+            int bytes_received = recv(socket_peer, read, 4096, 0);
+            if (bytes_received < 1) {
+                printf("Connection closed by peer.\n");
+                break;
+            }
+            printf("Received (%d bytes): %.*s",
+                    bytes_received, bytes_received, read);
+        }
+
+#if defined(_WIN32)
+        if(_kbhit()) {
+#else
+        if(FD_ISSET(0, &reads)) {
+#endif
+            char read[4096];
+            if (!fgets(read, 4096, stdin)) break;
+            printf("Sending: %s", read);
+            int bytes_sent = send(socket_peer, read, strlen(read), 0);
+            printf("Sent %d bytes.\n", bytes_sent);
+        }
+    } //end while(1)
+
+    printf("Closing socket...\n");
+    CLOSESOCKET(socket_peer);
+
+#if defined(_WIN32)
+    WSACleanup();
+#endif
+
+    printf("Finished.\n");
+    return 0;
+
+}
